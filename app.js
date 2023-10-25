fb20842d1760f6305c2274b7d5d2103beaec683b
boatconsider <boatzarock01@gmai.com>
2023-10-25 15:51:39 +0700

    d

--- app.js
+++ app.js
@@ -69,6 +69,19 @@ app.post('/login' ,jsonParser,function(req,res,next){
       );
 }
 )
+app.post('/rsmask', jsonParser, function (req, res, next) {
+  connection.execute(
+      'INSERT INTO rsmask (name, passwordsell, problem, img) VALUES (?, ?, ?, ?)',
+      [req.body.name, req.body.passwordsell, req.body.problem, req.body.img],
+      function (err, results, fields) {
+        if (err) {
+          res.json({ status: 'error', message: 'แจ้งปัญหาไม่สำเร็จ'});
+          return;
+        }
+        res.json({ status: 'ok', message: 'แจ้งปัญหาสำเร็จ' });
+      }
+    );
+});
 app.post('/rsmsdo', jsonParser, function (req, res, next) {
   connection.execute(
       'INSERT INTO rsmsdo (name, passwordsell, problem, img) VALUES (?, ?, ?, ?)',
