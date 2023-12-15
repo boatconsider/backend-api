7896d0c2e9e6b1c1464183ab577092d4d3997e46
boatconsider <boatzarock01@gmai.com>
2023-12-15 11:52:17 +0700

    f

--- functions/Notify.js
+++ functions/Notify.js
@@ -1,18 +1,19 @@
-const axios =require('axios')
+const axios = require('axios');
 
-exports.notifyLine =async(token,message)=>{
-try{
-const response = await axios({
-    method :"POST",
-    url:"https://notify-api.line.me/api/notify",
-    headers:{
-         'Content-Type':'application/x-www-form-urlencoded',
-         'Authorization':'Bearer '+token
-    },
-    data:'message'+message,
-})
-console.log("notify" ,response)
-}catch(err){
-console.log(err)
-}
-}
+exports.notifyLine = async (token, message) => {
+  try {
+    const response = await axios({
+      method: 'POST',
+      url: 'https://notify-api.line.me/api/notify',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Authorization': 'Bearer ' + token
+      },
+      data: 'message=' + message, // แก้เป็น 'message=' + message
+    });
+
+    console.log("notify", response.data); // แก้เป็น response.data
+  } catch (err) {
+    console.error(err);
+  }
+};
